ae79882c54f35c79c31dc51fc43a59b8fd1a18b6
chadrackmbala <chadrackmbala20@gmail.com>
2023-08-02 22:03:34 +0100

    ajout du dom pour recuperer les entrées

--- main.js
+++ main.js
@@ -1,27 +1,82 @@
-//mot de passe par defaut
- const motDepasseParDefaut=[1234,2007,1994,2003,'kadea']
+{
+  //mot de passe par defaut
+//  const motDepasseParDefaut=[1234,2007,1994,2003,'kadea']
 // const bouttonEnvoye= document.getElementById("envoye")
-
 
 // let  champDeSaisie = document.getElementById("Password")
 
+// function comparerValeurChamp() {
 
-function comparerValeurChamp() {
-  
-let  champDeSaisie = document.getElementById("Password").value;
-console.log(champDeSaisie);
+// let  champDeSaisie = document.getElementById("Password").value;
+// console.log(champDeSaisie);
+// }
+// comparerValeurChamp();
+
+/*Si mot de passe saisie est differance de
+ mot de passe  valide  retour " echec vous avez saisai un mauvais mot de pass"*/
+
+// alerte il vous reste 4 possibilite 
+// si echec retourn page votre compte est verouille contacter "L'admin"
 }
-comparerValeurChamp();
 
+const identifiants = ["Joseph", "Chadrack", "Djo", "Deborah"];
+const passwords = ["cj01", "drack02", "djo03", "debs04"];
+let isTrue;
 
+const connexionBtn = document.getElementById("connexion-btn");
 
+const afficheEntree = () => {
+  const inputUser = document.getElementById("user").value;
+  const inputPassword = document.getElementById("password").value;
+  alert(`Bonjour ${inputUser} votre mot de passe est ${inputPassword}`);
+  console.log(inputUser);
 
+  for(const identifiant of identifiants) {
+    if(inputUser === identifiant) {
+      console.log('Identifiant trouvé !');
+      isTrue = true;
+    } else {
+      console.log('Identifiant non retrouvé !');
+      isTrue = false;
+    }
+  }
+  for(const password of passwords) {
+    if(inputPassword === password) {
+      console.log('Mot de passe trouvé !');
+      isTrue = true;
+    } else {
+      console.log('Mot de passe non retrouvé !');
+      isTrue = false;
+    }
+  }
 
+  // if(isTrue) {
+  //   alert(`Bonjour ${inputUser} votre mot de passe est ${inputPassword}`);
+  // } else {
+  //   console.log('no')
+  // }
+}
 
+// const userSearch = () => {
+//   for(const identifiant of identifiants) {
+//     if(inputUser === identifiant) {
+//       console.log('Identifiant trouvé !');
+//       isTrue = true;
+//     } else {
+//       console.log('Identifiant non retrouvé !');
+//       isTrue = false;
+//     }
+//   }
+// }
 
+// const passwordSearch = () => {
+//   for(const password of passwords) {
+//     if(password === password) {
+//       console.log('Mot de passe trouvé !');
+//     } else {
+//       console.log('Mot de passe non retrouvé !');
+//     }
+//   }
+// }
 
-  /*Si mot de passe saisie est differance de
-   mot de passe  valide  retour " echec vous avez saisai un mauvais mot de pass"*/
-
-   // alerte il vous reste 4 possibilite 
-   // si echec retourn page votre compte est verouille contacter "L'admin"
+connexionBtn.addEventListener("click", afficheEntree);
