e9d70886e8203a667a368413c331b39a4d568ad9
Joseph basix <admin@MacBook-Pro.local>
2023-08-02 13:31:02 +0100

    Mon message de commit

---
+++ main.js
@@ -0,0 +1,27 @@
+//mot de passe par defaut
+ const motDepasseParDefaut=[1234,2007,1994,2003,'kadea']
+// const bouttonEnvoye= document.getElementById("envoye")
+
+
+// let  champDeSaisie = document.getElementById("Password")
+
+
+function comparerValeurChamp() {
+  
+let  champDeSaisie = document.getElementById("Password").value;
+console.log(champDeSaisie);
+}
+comparerValeurChamp();
+
+
+
+
+
+
+
+
+  /*Si mot de passe saisie est differance de
+   mot de passe  valide  retour " echec vous avez saisai un mauvais mot de pass"*/
+
+   // alerte il vous reste 4 possibilite 
+   // si echec retourn page votre compte est verouille contacter "L'admin"
